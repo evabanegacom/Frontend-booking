bb34f3615a99e19257f26d458bd5c8daa21ed4f8
Precious chiedu <udegbue69@gmail.com>
2020-11-26 09:46:26 +0100

    added links

--- src/components/bikeList.js
+++ src/components/bikeList.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const BikeList = () => (
   <div>
     <h1>Bike List goes here</h1>
+    <Link to="/bike/:id">Bike Info</Link>
   </div>
 );
 export default BikeList;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SignedInLInks from '../container/signInLink';
 import SignedOutLinks from '../container/signOutLInk';
 
 const Navbar = () => (
   <div>
     <h5>Navbar goes here</h5>
+    <Link to="/bikes">Bikes</Link>
+    <Link to="/user/:id/:bookings">Bookings</Link>
     <SignedInLInks />
     <SignedOutLinks />
   </div>
