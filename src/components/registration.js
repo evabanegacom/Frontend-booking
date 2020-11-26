f8ebb3b59144ca9873ed52f4a8edd74dc2849da7
Precious chiedu <udegbue69@gmail.com>
2020-11-26 21:39:01 +0100

    created user api

--- src/components/registration.js
+++ src/components/registration.js
@@ -1,59 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const SignUp = () => (
-  <div className="container">
-    <form className="white">
-      <h5 className="grey-text text-darken-3">Register</h5>
-      <div className="input-field">
-        <label htmlFor="email">
-          Email
-          <input
-            id="email"
-            autoComplete="off"
-            required
-            type="email"
-          />
-        </label>
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      name: '',
+      password: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit = e => {
+    const { name, email, password } = this.state;
+    axios.post('http://localhost:3001/api/v1/users', {
+      name,
+      email,
+      password,
+    });
+    e.preventDefault();
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">Register</h5>
+          <div className="input-field">
+            <label htmlFor="name">
+              Name
+              <input
+                id="name"
+                autoComplete="off"
+                required
+                type="text"
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          <div className="input-field">
+            <label htmlFor="email">
+              Email
+              <input
+                id="email"
+                autoComplete="off"
+                required
+                type="email"
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">
+              Password
+              <input
+                id="password"
+                required
+                autoComplete="off"
+                type="password"
+                onChange={this.handleChange}
+              />
+            </label>
+          </div>
+          <div className="input-field">
+            <button type="submit" className="btn pink lighten-1 z-depth-0">
+              Register
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="input-field">
-        <label htmlFor="password">
-          Password
-          <input
-            id="password"
-            autoComplete="off"
-            required
-            type="password"
-          />
-        </label>
-      </div>
-      <div className="input-field">
-        <label htmlFor="firstName">
-          FirstName
-          <input
-            id="firstName"
-            required
-            type="text"
-          />
-        </label>
-      </div>
-      <div className="input-field">
-        <label htmlFor="lastName">
-          Last Name
-          <input
-            id="lastName"
-            required
-            autoComplete="off"
-            type="text"
-          />
-        </label>
-      </div>
-      <div className="input-field">
-        <button type="submit" className="btn pink lighten-1 z-depth-0">
-          Register
-        </button>
-      </div>
-    </form>
-  </div>
-);
+    );
+  }
+}
 
 export default SignUp;
