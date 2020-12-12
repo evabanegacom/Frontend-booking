e97c729150cee2abfd2d290d5daa29d3139c4e87
Precious chiedu <udegbue69@gmail.com>
2020-12-12 10:37:28 +0100

    added selection features

--- src/container/carInfo.js
+++ src/container/carInfo.js
@@ -2,31 +2,36 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
+import { getCarDetails, getCars } from '../actions/actions';
+import Booking from './booking';
+import BikeInfo from '../components/bikeInfo';
+import '../cssFiles/carInfo.css';
 /* eslint-disable */
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import { getCarDetails, getCars } from "../actions/actions";
-import Booking from "./booking";
-import BikeInfo from "../components/bikeInfo";
-import "../cssFiles/carInfo.css";
-
 class CarInfo extends Component {
   componentDidMount() {
     const { details, theCars } = this.props;
     const { match } = this.props;
     const { params } = match;
     const { id } = params;
-    console.log(this.props)
     details(id);
     theCars();
-    // const carId = this.props.match.params.id
+  }
+
+  componentDidUpdate() {
+    const { details } = this.props;
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    details(id);
   }
 
   render() {
     const { carDetails, cars } = this.props;
     const slideCars = cars.length ? (
-      cars.map((car) => (
+      cars.map(car => (
         <div key={car.id}>
           <BikeInfo car={car} key={car.id} />
         </div>
@@ -37,37 +42,40 @@ class CarInfo extends Component {
       </div>
     );
     const revealForm = () => {
-      document.querySelector(".form-con").classList.remove("hide");
-      document.querySelector(".carDesign").classList.add("hide");
-      document.querySelector(".carDesign").classList.remove("carDesign");
+      document.querySelector('.form-con').classList.remove('hide');
+      document.querySelector('.carDesign').classList.add('hide');
+      document.querySelector('.carDesign').classList.remove('carDesign');
     };
-    const carUrl = carDetails.avatar ? carDetails.avatar.url : "";
+    const carUrl = carDetails.avatar ? carDetails.avatar.url : '';
     const theCar = !carDetails.length ? (
       <div className="carInfos">
         <div className="carousel">
-          <Slider className="slider"
+          <Slider
+            className="slider"
             dots={false}
             slidesToShow={4}
             slidesToScroll={2}
-            autoplay={true}
+            autoplay
             autoplaySpeed={3000}
           >
             {slideCars}
           </Slider>
-          <Slider className="sliders"
+          <Slider
+            className="sliders"
             dots={false}
             slidesToShow={1}
             slidesToScroll={2}
-            autoplay={true}
+            autoplay
             autoplaySpeed={3000}
           >
             {slideCars}
           </Slider>
-          <Slider className="slider2"
+          <Slider
+            className="slider2"
             dots={false}
             slidesToShow={1}
             slidesToScroll={2}
-            autoplay={true}
+            autoplay
             autoplaySpeed={3000}
           >
             {slideCars}
@@ -119,13 +127,13 @@ class CarInfo extends Component {
     );
   }
 }
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   carDetails: state.carDetailsReducer.car,
   cars: state.carReducer.cars,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  details: (id) => dispatch(getCarDetails(id)),
+const mapDispatchToProps = dispatch => ({
+  details: id => dispatch(getCarDetails(id)),
   theCars: () => {
     dispatch(getCars());
   },
@@ -136,11 +144,12 @@ CarInfo.propTypes = {
   carDetails: PropTypes.arrayOf.isRequired,
   match: PropTypes.string.isRequired,
   params: PropTypes.string.isRequired,
+  cars: PropTypes.arrayOf.isRequired,
+  theCars: PropTypes.func.isRequired,
 };
 
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(withRouter(CarInfo));
-
 /* eslint-enable */
